af58747ccec63a46237f6443bfbeb8723d030bb3
sucramdev <marcusvonschantz@gmail.com>
2026-09-06 00:11:41 +0200

    feat: integrate Auth0 authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,23 @@
+import { useAuth0 } from "@auth0/auth0-react"
 import { Link } from "react-router-dom"
+
 
 function Home() {
 
+    const {isAuthenticated, user, logout} = useAuth0()
+
     return<>
-    
-    
     <h1>Lalynk</h1>
-    <Link to="/login">Login</Link>
+    {isAuthenticated && (
+    <>
+        <p>{user?.email}</p>
+        <button onClick={() => logout()}>Log out</button> 
+    </>
+    )}
+
+    {!isAuthenticated && (
+        <Link to="/login">Login</Link>
+    )}
     </>
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,19 @@
+import { useAuth0 } from "@auth0/auth0-react"
+
+
+
+
+
 function Login() {
 
-    return <><h1>Login</h1></>
+    const {loginWithRedirect} = useAuth0()
+
+    return <>
+    <h1>Login</h1>
+    <button onClick={() => loginWithRedirect()}>Log in</button>
+    
+    
+    </>
 }
 
 export default Login
