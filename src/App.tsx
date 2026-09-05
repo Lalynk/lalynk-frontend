a13103bf7d62f70d25505b6248fc9b415c7d124f
sucramdev <marcusvonschantz@gmail.com>
2026-09-05 23:23:46 +0200

    feat: add frontend routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
   return (
     <>
-     <h1>Lalynk</h1>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<Home></Home>}></Route>
+        <Route path="/login" element= {<Login></Login>}></Route>
+        <Route path="/dashboard" element= {<Dashboard></Dashboard>}></Route>
+      </Routes>
+     </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function Home() {
+
+    return<>
+    
+    
+    <h1>Lalynk</h1>
+    <Link to="/login">Login</Link>
+    </>
+}
+
+export default Home
