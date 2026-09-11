640f98149e531ce0228cb51f4c7808287a74b4de
sucramdev <marcusvonschantz@gmail.com>
2026-09-11 19:54:08 +0200

    feat: remove auth0 dependency and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
      <BrowserRouter>
       <Routes>
         <Route path="/" element= {<Home></Home>}></Route>
-        <Route path="/login" element= {<Login></Login>}></Route>
         <Route path="/dashboard" element= {<Dashboard></Dashboard>}></Route>
       </Routes>
      </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,10 @@
-import { useAuth0 } from "@auth0/auth0-react"
+
 import { Link } from "react-router-dom"
 
 
 function Home() {
 
-    const {isAuthenticated, user, logout} = useAuth0()
-
-    return<>
-    <h1>Lalynk</h1>
-    {isAuthenticated && (
-    <>
-        <p>{user?.email}</p>
-        <button onClick={() => logout()}>Log out</button> 
-    </>
-    )}
-
-    {!isAuthenticated && (
-        <Link to="/login">Login</Link>
-    )}
-    </>
+    return (<></>)
 }
 
 export default Home
